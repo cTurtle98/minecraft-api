02b9d6437ea415a6533ef3a29852340ba4db8263
Ciaran Farley <ciaran@cturtle98.com>
2020-06-10 21:02:20 -0700

    v1.0.0

---
+++ minecraft-api.py
@@ -0,0 +1,40 @@
+"""
+Minecraft API
+By Ciaran Farley ciaran@cturtle98.com
+
+Version 1.0.0
+
+main file
+"""
+
+# import stuff
+from flask import Flask
+from flask import request
+import os
+
+#vars
+SCREEN_NAME = minecraft
+
+#setup flask
+app = Flask(__name__)
+
+#enable flask debug
+app.config["DEBUG"]
+
+@app.route('/')
+def home():
+	return ''' \
+	you have reached cturtle98 minecraft api
+	see https://github.com/cTurtle98/minecraft-api
+	'''
+
+@app.route('/whitelist/add/' methods=['POST'])
+def whitelist_add():
+
+	# get the minecraft username from the request
+	uname = request.args.get('u')
+
+	os.system("screen -S " + SCREEN_NAME + " -X stuff 'whitelist add " + uname + "\015")
+
+# run flask
+@app.run()
