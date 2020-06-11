b478d7c5957e760275be02361d3d7c974036e2f7
Ciaran Farley <ciaran@cturtle98.com>
2020-06-11 08:42:13 -0700

    Update minecraft-api.py

--- minecraft-api.py
+++ minecraft-api.py
@@ -2,7 +2,7 @@
 Minecraft API
 By Ciaran Farley ciaran@cturtle98.com
 
-Version 1.0.5
+Version 1.0.6
 
 main file
 """
@@ -28,13 +28,20 @@ def home():
 	see https://github.com/cTurtle98/minecraft-api
 	'''
 
-@app.route('/whitelist/add/', methods=['POST'])
+@app.route('/whitelist/add/', methods=['POST', 'GET'])
 def whitelist_add():
 
 	# get the minecraft username from the request
 	uname = request.args.get('u')
-
+	
+	if (app.config["DEBUG"] == True):
+	    print("WHITELIST REQUEST: " + uname)
+	
+	#send the command to the minecraft server
 	os.system("screen -S " + SCREEN_NAME + " -X stuff \'whitelist add " + uname + "\015\'")
+	
+	#return ok
+	return 200
 
 # run flask
 app.run(port=8080)
